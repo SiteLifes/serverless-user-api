a7e0ae853946c3bc49ba7d9e175d00e523513202
seferbulbul <bulbul.sefer@gmail.com>
2025-09-16 23:51:54 +0300

    ModifiedAt is nullable test.

--- src/Domain/Entities/UserDeviceEntity.cs
+++ src/Domain/Entities/UserDeviceEntity.cs
@@ -11,7 +11,7 @@ public class UserDeviceEntity : IEntity
     [JsonPropertyName("userId")] public string UserId { get; set; } = default!;
     [JsonPropertyName("platform")] public string? Platform { get; set; }
     [JsonPropertyName("createdAt")] public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-    [JsonPropertyName("modifiedAt")] public DateTime ModifiedAt { get; set; } = DateTime.UtcNow;
+    [JsonPropertyName("modifiedAt")] public DateTime ? ModifiedAt { get; set; } = DateTime.UtcNow;
     [JsonPropertyName("additionalData")] public Dictionary<string, string> AdditionalData { get; set; } = new();
     
     public static string GetPk(string userId)
